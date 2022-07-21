bc740f28d32e5b5beae8c61b4553c0d2a1c5932b
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-07-21 16:50:29 -0500

    Tree Project

---
+++ src/Tree.java
@@ -0,0 +1,13 @@
+public class Tree{
+    public static void main(String[] args){
+
+        System.out.printf("Time to plant a tree!\n\n");
+
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.printf("  *  \n");
+
+    }
+}
